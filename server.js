b01ab06b9837f89033cfa652a03e5e55e9af4477
laytonglee <laytongly@gmail.com>
2026-04-28 09:28:12 +0700

    config server.js

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
-const express = require("express");
-const app = express();
-const port = 3000;
+require("dotenv").config();
+const app = require("./app");
+const connectDB = require("./src/config/db");
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+const PORT = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+const start = async () => {
+  await connectDB();
+  app.listen(PORT, () => {
+    console.log(
+      `[SERVER] Running on port ${PORT} in ${process.env.NODE_ENV || "development"} mode`,
+    );
+    console.log(
+      `[SERVER] URL: ${process.env.APP_URL || `http://localhost:${PORT}`}`,
+    );
+  });
+};
+
+start();
